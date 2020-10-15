834ae52c3c86617d0a301fd94beef18122646b9b
Chad Smith <chad.e.smith@outlook.com>
2020-10-15 18:34:36 -0500

    make card clickable

--- portfolio/src/app/content/social-media/social-media.component.ts
+++ portfolio/src/app/content/social-media/social-media.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-social-media',
   templateUrl: './social-media.component.html',
   styleUrls: ['./social-media.component.css']
 })
-export class SocialMediaComponent implements OnInit {
+export class SocialMediaComponent {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
-  }
 
 }
